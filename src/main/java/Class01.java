fed9d0202695ec63470c606a6188adcc537f3de3
almaliogluis <almalioglu@gmail.com>
2023-05-12 20:34:42 +0300

    version1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,10 @@
+public class Class01 {
+    public static void main(String[] args) {
+        int x =3;
+        int y =5;
+        System.out.println(x+y);
+        System.out.println("Version1 icin islemler tamam");
+
+
+    }
+}
